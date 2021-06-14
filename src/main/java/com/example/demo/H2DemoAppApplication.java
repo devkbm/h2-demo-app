95ff4aee8bdede94b0342efd4a4b8936c9be48a5
cb457 <cb457@IE-NG-0760.snubh.com>
2021-06-14 17:51:21 +0900

    first commit

---
+++ src/main/java/com/example/demo/H2DemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2DemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2DemoAppApplication.class, args);
+	}
+
+}
